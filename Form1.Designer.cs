ab0429fe56bafd2b50539a1ea4410ba793f25610
Zachary Burns <zburns@katzmidas.com>
2018-04-13 15:03:58 -0400

    Check In from Old Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SlideShow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelFolderToShow = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select &Photos Folder to Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folder to Show";
+            // 
+            // labelFolderToShow
+            // 
+            this.labelFolderToShow.Location = new System.Drawing.Point(112, 54);
+            this.labelFolderToShow.Name = "labelFolderToShow";
+            this.labelFolderToShow.Size = new System.Drawing.Size(488, 13);
+            this.labelFolderToShow.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Seconds to Show Photo";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(367, 15);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(447, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Play &Music Found in Folder";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Press ALT-F4 to terminate slide show.";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(414, 73);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(187, 23);
+            this.buttonStart.TabIndex = 9;
+            this.buttonStart.Text = "&Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox.Location = new System.Drawing.Point(0, 99);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(612, 10);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 10;
+            this.pictureBox.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(16, 76);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(125, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "&Loop Photos Forever";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 109);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelFolderToShow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slide Show";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelFolderToShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SlideShow
+{
+    public partial class Form1 : Form
+    {
+        string foldertoshow = null;
+        string[] files = null;
+        string[] music = null;
+        int currentfile = 0;
+        System.Windows.Forms.Timer timer = new Timer();
+        WMPLib.WindowsMediaPlayer wplayer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            foldertoshow = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\";
+            labelFolderToShow.Text = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderdialog = new FolderBrowserDialog();
+            folderdialog.Description = "Select folder to show";
+            if (folderdialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foldertoshow = folderdialog.SelectedPath;
+                labelFolderToShow.Text = folderdialog.SelectedPath;
+            }
+            else
+            {
+                foldertoshow = null;
+                labelFolderToShow.Text = "";
+            }
+            folderdialog.Dispose();
+            folderdialog = null;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (foldertoshow != null)
+            {
+                pictureBox.Dock = DockStyle.Fill;
+                files = getFiles(foldertoshow, "*.gif|*.jpg|*.png|*.bmp", System.IO.SearchOption.TopDirectoryOnly);
+                
+
+                this.WindowState = FormWindowState.Maximized;
+                this.FormBorderStyle = FormBorderStyle.None;
+                //this.TopMost = true;
+                this.Bounds = System.Windows.Forms.Screen.GetBounds(this);
+
+                timer.Interval = Convert.ToInt32(numericUpDown1.Value) * 1000;
+                timer.Tick += timer_Tick;
+                timer.Start();
+                try
+                {
+                    if (checkBox1.Checked)
+                    {
+                        music = getFiles(foldertoshow, "*.mp3", System.IO.SearchOption.TopDirectoryOnly);
+                        if (music.Length > 0)
+                        {
+                            //play music
+                            wplayer = new WMPLib.WindowsMediaPlayer();
+                            wplayer.URL = music[0];
+                            wplayer.settings.setMode("Loop", true);
+                            wplayer.controls.play();
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public string[] getFiles(string SourceFolder, string Filter, System.IO.SearchOption searchOption)
+        {
+            // ArrayList will hold all file names
+            System.Collections.ArrayList alFiles = new System.Collections.ArrayList();
+
+            // Create an array of filter string
+            string[] MultipleFilters = Filter.Split('|');
+
+            // for each filter find mathing file names
+            foreach (string FileFilter in MultipleFilters)
+            {
+                // add found file names to array list
+                alFiles.AddRange(System.IO.Directory.GetFiles(SourceFolder, FileFilter, searchOption));
+            }
+
+            // returns string array of relevant file names
+            return (string[])alFiles.ToArray(typeof(string));
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            try
+            {
+                pictureBox.Image = System.Drawing.Image.FromFile(files[currentfile]);
+                currentfile++;
+            }
+            catch
+            {
+                currentfile = 0;
+                if (!checkBox2.Checked)
+                    this.Close();
+            }
+            timer.Enabled = true;
+        }
+    }
+}
